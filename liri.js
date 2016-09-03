674fe68641d26b6331058345b0707ff0bd1db1dd
Melissa Garcia <melissag13@gmail.com>
2016-09-02 23:14:06 -0400

    add variables

---
+++ liri.js
@@ -0,0 +1,23 @@
+var keys = require("./keys.js");
+var fs = require("fs");
+
+var twitter = require("twitter");
+var spotify = require("spotify");
+var request = require("request");
+
+function my-tweets(){
+	
+};
+
+function spotify-this-song(){
+
+};
+
+function movie-this(){
+	
+};
+
+function do-what-it-says(){
+
+};
+

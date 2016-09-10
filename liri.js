a525433e79acefe0d81523ea8e7a481c7b4f3a17
Melissa Garcia <melissag13@gmail.com>
2016-09-09 20:00:41 -0400

    work work working

--- liri.js
+++ liri.js
@@ -3,25 +3,115 @@ var keys = require("./keys.js");
 
 var Twitter = require("twitter");
 
+var spotify = require('spotify');
+
+var request = require('request');
+
+var fs = require("fs");
+
+// Put this in a function so it doesn't automatically runs, only when you call it
+var getTweets = function() {
+
 var client = new Twitter(keys.twitterKeys);
 
-var params = {screen_name: "melissag131"};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    // console.log(tweets);
-    for(var i=0; i<tweets.length; i++) {
-    	// tweets[i] = whatever tweet position you are at
-    	console.log(tweets[i].created_at);
-      console.log('');
-      console.log(tweets[i].text);
-     
+    var params = {screen_name: "melissag131"};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+        // console.log(tweets);
+            //loop through all of the tweets
+            for(var i=0; i<tweets.length; i++) {
+                // tweets[i] = whatever tweet position you are at
+                console.log(tweets[i].created_at);
+                console.log('');
+                console.log(tweets[i].text);
+              
+             }
+          }
+    });
+}
+
+var getArtist = function(artist) {
+    return artist.name;
+}
+var getSpotify = function(songName) {
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+
+        // Loops through songs and pulls out attributes to display only the info we want user to see
+       var songs = data.tracks.items;
+            for(var i = 0; i < songs.length; i++){
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtist));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+        }
+
+    });
+}
+
+var getMovie = function(movieName) {
+    request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json", function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var jsonData = JSON.parse(body);
+
+            console.log("Title of the movie: " + jsonData.Title);
+            console.log("Year it came out: " + jsonData.Year);
+            console.log("Rated: " + jsonData.Rated);
+            console.log("IMDB Rating: " + jsonData.imdbRating);
+            console.log("Country where produced: " + jsonData.Country);
+            console.log("Language of movie: " + jsonData.Language);
+            console.log("Plot of the movie: " + jsonData.Plot);
+            console.log("Actors in the movie: " + jsonData.Actors);
+            console.log("Rotten Tomatoes Rating: " + jsonData.tomatoRating);
+            console.log("Rotten Tomatoes URL: " + jsonData.tomatoURL);
+
+             
+        }
+    });
+}
+
+var doWhatItSays = function() {
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if (error) 
+            throw error;
+        // We will then print the contents of data
+        console.log(data);
+        var dataArr = data.split(",");
+            if(dataArr.length == 2) {
+                pick(dataArr[0], dataArr[1]);
+            } else if (dataArr.length == 1) {
+                pick(dataArr[0]);
+            }
+    });
+}
+var pick = function(caseData, functionData) {
+    switch(caseData) {
+        case "my-tweets":
+            getTweets();
+            break;
+        case "spotify-this-song":
+            getSpotify(functionData);
+            break;
+        case "movie-this":
+            getMovie(functionData);
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
+        
     }
-  }
-});
+}
 
+var runThis = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+};
 
-
-
+runThis(process.argv[2], process.argv[3]);
 
 
 
